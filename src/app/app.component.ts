f6e2a349f3bcc8625cf7f2dfba184dbd017a327f
Gian MIer <giansebas.2015@gmail.com>
2024-10-13 11:20:05 -0500

    Add: home panel page and change primeng dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule  } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { ToolbarModule } from 'primeng/toolbar';
-import { InputTextModule } from 'primeng/inputtext';
+import { definePreset } from 'primeng/themes';
+import { Aura } from 'primeng/themes/aura';
+import { PrimeNGConfig } from 'primeng/api';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, CardModule, ToolbarModule, InputTextModule],
+  imports: [RouterOutlet, TopBarComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'catalogo-app-new';
+  public config: PrimeNGConfig = inject(PrimeNGConfig);
+
+  constructor() {
+    this.config.theme.set({preset: Noir});
 }
 
+}
+
+
+
+const Noir = definePreset(Aura, {
+  semantic: {
+      primary: {
+          50: '{zinc.50}',
+          100: '{zinc.100}',
+          200: '{zinc.200}',
+          300: '{zinc.300}',
+          400: '{zinc.400}',
+          500: '{zinc.500}',
+          600: '{zinc.600}',
+          700: '{zinc.700}',
+          800: '{zinc.800}',
+          900: '{zinc.900}',
+          950: '{zinc.950}'
+      },
+      colorScheme: {
+          light: {
+              primary: {
+                  color: '{zinc.950}',
+                  inverseColor: '#ffffff',
+                  hoverColor: '{zinc.900}',
+                  activeColor: '{zinc.800}'
+              },
+              highlight: {
+                  background: '{zinc.950}',
+                  focusBackground: '{zinc.700}',
+                  color: '#ffffff',
+                  focusColor: '#ffffff'
+              }
+          },
+          dark: {
+              primary: {
+                  color: '{zinc.50}',
+                  inverseColor: '{zinc.950}',
+                  hoverColor: '{zinc.100}',
+                  activeColor: '{zinc.200}'
+              },
+              highlight: {
+                  background: 'rgba(250, 250, 250, .16)',
+                  focusBackground: 'rgba(250, 250, 250, .24)',
+                  color: 'rgba(255,255,255,.87)',
+                  focusColor: 'rgba(255,255,255,.87)'
+              }
+          }
+      }
+  }
+});
