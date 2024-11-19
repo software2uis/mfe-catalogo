c3fac1c83048b66bf8076af8ed4ca50c8c95cc21
bubumonda <115027371+bubumonda@users.noreply.github.com>
2024-11-19 13:05:57 -0500

    Fix products query mf (#25)

--- src/app/components/home-panel/home-panel.component.ts
+++ src/app/components/home-panel/home-panel.component.ts
@@ -2,10 +2,9 @@ import { Component, inject } from '@angular/core';
 import { SplitterModule } from 'primeng/splitter';
 import { FilterPanelComponent } from '../filter-panel/filter-panel.component';
 import { ProductsPanelComponent } from '../products-panel/products-panel.component';
-import { ProductComponent } from '../product/product.component';
-import { catchError, of, tap } from 'rxjs';
 import { ProductsService } from '../../services/products.service';
 import { ResponsePaginated } from '../../models/paginate.interface';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-home-panel',
@@ -15,7 +14,6 @@ import { ResponsePaginated } from '../../models/paginate.interface';
   styleUrl: './home-panel.component.scss'
 })
 export class HomePanelComponent {
-  updateDataHandlerSuggestion:any;
   updateDataHandlerQuery:any;
 
   productsService:ProductsService = inject(ProductsService);
