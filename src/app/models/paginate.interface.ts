f544436a2f6fa7661605583dd4e1f8774ebc1573
Majinka10 <camachopinto_@hotmail.com>
2024-11-23 19:01:09 -0500

    Refactor pagination interface and improve products service methods

--- src/app/models/paginate.interface.ts
+++ src/app/models/paginate.interface.ts
@@ -1,34 +1,30 @@
-import { Product } from "./product.interface";
+import { Product } from './product.interface';
 
-export interface ResponsePaginated {
-  content:          Product[];
-  pageable:         Pageable;
-  totalElements:    number;
-  totalPages:       number;
-  last:             boolean;
-  size:             number;
-  number:           number;
-  sort:             Sort;
+export interface ResponsePaginated<T> {
+  content: T[];
+  pageable: Pageable;
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: Sort;
   numberOfElements: number;
-  first:            boolean;
-  empty:            boolean;
+  first: boolean;
+  empty: boolean;
 }
-
-
-
-
 
 export interface Pageable {
   pageNumber: number;
-  pageSize:   number;
-  sort:       Sort;
-  offset:     number;
-  paged:      boolean;
-  unpaged:    boolean;
+  pageSize: number;
+  sort: Sort;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
 }
 
 export interface Sort {
-  sorted:   boolean;
-  empty:    boolean;
+  sorted: boolean;
+  empty: boolean;
   unsorted: boolean;
 }
